2e699e3b2db97bc78cd205768949ad8023ce36b9
john meechan <john2.meechan@live.uwe.ac.uk>
2021-08-28 19:07:28 +0100

    Camera + Collision + Tiles
    Made the camera follow the player on movement, player collision is now sorted through boxcasts, and several tiles/sprites have been made using the sprite editor

--- Top Down RPG/Assets/CameraMotor.cs
+++ Top Down RPG/Assets/CameraMotor.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class CameraMotor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform lookAt;
+
+    public float boundX = 0.15f;
+    public float boundY = 0.10f;
+
+    private void LateUpdate()
     {
-        
+        Vector3 delta = Vector3.zero;
+
+        //checks if we are inside the bounds on the x axis
+        float deltaX = lookAt.position.x - transform.position.x;
+        if(deltaX > boundX || deltaX < -boundX)
+        {
+            //if the camera is left of the player
+            if(transform.position.x < lookAt.position.x)
+            {
+                //updates the x value of the delta vector, to be used for camera movement
+                delta.x = deltaX - boundX;
+            }
+            //if the camera is right of the player
+            else
+            {
+                delta.x = deltaX + boundX;
+            }
+        }
+
+        //checks if we are inside the bounds on the y axis
+        float deltaY = lookAt.position.y - transform.position.y;
+        if (deltaY > boundY || deltaY < -boundY)
+        {
+            if (transform.position.y < lookAt.position.y)
+            {
+                //updates the y value of the delta vector, to be used for camera movement
+                delta.y = deltaY - boundY;
+            }
+            else
+            {
+                delta.y = deltaY + boundY;
+            }
+        }
+        //updates the position of the camera using the delta vector values calculated in the if statements
+        transform.position += new Vector3(delta.x, delta.y, 0);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Top Down RPG/Assets/PlayerController.cs
+++ Top Down RPG/Assets/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
 
     private BoxCollider2D boxCollider;
     private Vector3 moveDelta;
+    private RaycastHit2D hit;
 
     // Start is called before the first frame update
     void Start()
@@ -34,7 +35,21 @@ public class PlayerController : MonoBehaviour
             transform.localScale =new Vector3(-1, 1, 1);
         }
 
-        //makes sprite move
-        transform.Translate(moveDelta * Time.deltaTime);
+        //makes sure we can move on y axis by casting box ahead of us
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if(hit.collider == null)
+        {
+            //makes sprite move
+            transform.Translate(0,moveDelta.y * Time.deltaTime,0);
+        }
+
+        //makes sure we can move on the x axis by casting a box ahead of us
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (hit.collider == null)
+        {
+            //makes sprite move
+            transform.Translate(moveDelta.x * Time.deltaTime, 0,0);
+        }
+
     }
 }
